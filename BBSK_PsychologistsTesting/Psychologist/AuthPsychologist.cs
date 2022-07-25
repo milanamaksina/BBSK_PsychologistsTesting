4be843e2f877be4032c371e52c626b356e89a59b
Milana Maksina <m_maksinaa@mail.ru>
2022-07-25 21:14:03 +0300

    Add authpsho

--- BBSK_PsychologistsTesting/Psychologist/AuthPsychologist.cs
+++ BBSK_PsychologistsTesting/Psychologist/AuthPsychologist.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BBSK_PsychologistsTesting.Psychologist
 {
     public class AuthPsychologist
     {
+        public HttpResponseMessage Authorize(AuthRequestModel model)
+        {
+            string json = JsonSerializer.Serialize(model);
+
+            HttpClient psychologist = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri(Urls.Auth),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            return psychologist.Send(message);
+        }
     }
 }
---
+++ BBSK_PsychologistsTesting/Urls.cs
@@ -0,0 +1,10 @@
+namespace BBSK_PsychologistsTesting
+{
+    public class Urls
+    {
+        public const string Host = $"https://piter-education.ru:10040";
+
+        public const string Psychologists = $"{Host}/Psychologists";
+        public const string Auth = $"{Host}/Auth";
+    }
+}
