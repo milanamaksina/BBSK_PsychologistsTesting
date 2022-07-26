a2b09fb48b41cbf40a40166d0f57d1af7fd2d5cc
Milana Maksina <m_maksinaa@mail.ru>
2022-07-26 23:30:32 +0300

    Add registration

---
+++ BBSK_PsychologistsTesting/Psychologist/PsychologistsPsychologist.cs
@@ -0,0 +1,50 @@
+﻿using BBSK_PsychologistsTesting.Models.Request;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BBSK_PsychologistsTesting.Psychologist
+{
+    public class PsychologistsPsychologist
+    {
+        public HttpResponseMessage RegisterPsychologist(PsychologistRequestModel model)
+        {
+            string json = JsonSerializer.Serialize(model);
+
+            HttpClient psychologist = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri(Urls.Psychologists),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            return psychologist.Send(message);
+        }
+
+        public HttpContent GetPsychologistById(int id, string token, HttpStatusCode expectedCode)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new System.Uri($"{Urls.Psychologists}/{id}")
+            };
+            HttpResponseMessage httpResponse = client.Send(message);
+            HttpStatusCode actualCode = httpResponse.StatusCode;
+
+            Assert.AreEqual(expectedCode, actualCode);
+
+            return httpResponse.Content;
+        }
+    }
+}
