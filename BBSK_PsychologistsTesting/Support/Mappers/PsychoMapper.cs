26d648ffac806fddb611934f00d6fccd27faafbf
Milana Maksina <m_maksinaa@mail.ru>
2022-08-02 19:42:01 +0300

    change: updatePsychologist with mapper

---
+++ BBSK_PsychologistsTesting/Support/Mappers/PsychoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BBSK_PsychologistsTesting.Models.Request;
+using BBSK_PsychologistsTesting.Models.Response;
+
+namespace BBSK_PsychologistsTesting.Support.Mappers
+{
+    public class PsychoMapper
+    {
+        public PsychologistResponseModel MappPsychologistRequestModelToPsychologistResponseModel(PsychologistRequestModel model, int id)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<PsychologistResponseModel, PsychologistRequestModel>());
+            Mapper mapper = new Mapper(config);
+            var responseModel = mapper.Map<PsychologistResponseModel>(model);
+            responseModel.Id = id;
+            return responseModel;
+        }
+    }
+}
