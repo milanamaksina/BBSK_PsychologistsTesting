2d911557f87fc9984cafc357145d206c87e7e69b
Anastasiia <vostrova_1994@mail.ru>
2022-08-07 18:19:00 +0300

    Add mapp, steps

---
+++ BBSK_PsychologistsTesting/Support/Mappers/SearchRequestsMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BBSK_PsychologistsTesting.Models.Request;
+using BBSK_PsychologistsTesting.Models.Response;
+
+namespace BBSK_PsychologistsTesting.Support.Mappers
+{
+    public class SearchRequestsMapper
+    {
+        public SearchRequestsResponseModel MappSearchRequestsRequestsModelToSearchRequestsResponseModel(SearchRequestsRequestsModel searchRequestsRequestsModel, int id)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<SearchRequestsResponseModel, SearchRequestsRequestsModel>());
+            Mapper mapper = new Mapper(config);
+            var responseModel = mapper.Map<SearchRequestsResponseModel>(searchRequestsRequestsModel);
+            responseModel.Id = id;
+            return responseModel;
+        }
+    }
+}
---
+++ BBSK_PsychologistsTesting/Tests/TestSources/CreateClientSearchRequestsTestSources.cs
@@ -0,0 +1,31 @@
+﻿using BBSK_PsychologistsTesting.Models.Request;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBSK_PsychologistsTesting.Tests.TestSources
+{
+    public class CreateClientSearchRequestsTestSources
+    {
+        public class SearchRequests_WhenSearchRequestsIsCorrect_TestSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new SearchRequestsRequestsModel()
+                {
+                    Name = "Вася",
+                    PhoneNumber = "8888888888",
+                    Description = "Тот что не дурак, а дурак не нужен",
+                    PsychologistGender = 0,
+                    CostMin = 0,
+                    CostMax = 5000,
+                    Date = new DateTime(2022, 08, 05),
+                    Time = 1                  
+                };
+            }
+        }
+    }
+}
